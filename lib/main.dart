ae44f9efd586967a7cac3062bb54d15d1dc8f852
MasahiroLittleforest <masahiro.littleforest@gmail.com>
2019-10-14 18:32:47 +0900

    highlight text

--- lib/main.dart
+++ lib/main.dart
@@ -3,39 +3,20 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Highlight Text Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Highlight Text Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -44,68 +25,192 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  String originalText = '';
+  String inputText = '';
+  String highlightedText = '';
+  final TextEditingController _originalTextController = TextEditingController();
+  final TextEditingController _highlightedPartController =
+      TextEditingController();
 
-  void _incrementCounter() {
+  void refreshScreen() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      originalText = '';
+      inputText = '';
+      highlightedText = '';
+      _originalTextController.clear();
+      _highlightedPartController.clear();
     });
+  }
+
+  List<Widget> getHighlightedText(String originalString, String inputString) {
+    List<Widget> highlightedText;
+    final String lowerOriginalString = originalString.toLowerCase();
+    final String lowerInputString = inputString.toLowerCase();
+    final int lastOfOriginalString = originalString.length - 1;
+    final int firstOfInputString =
+        lowerOriginalString.indexOf(lowerInputString);
+    int lastOfInputString;
+
+    // 入力値に連続する同じ文字が含まれる時に、'lastOfInputString'が1つ目のインデックスにならないようにする
+    if (lowerInputString.length >= 2) {
+      final String secondLastInputLetter =
+          lowerInputString[lowerInputString.length - 2];
+      final String lastInputLetter =
+          lowerInputString[lowerInputString.length - 1];
+      if (secondLastInputLetter == lastInputLetter) {
+        lastOfInputString = lowerOriginalString.indexOf(lowerInputString) +
+            (lowerInputString.length - 1);
+      } else {
+        lastOfInputString = lastOfInputString = lowerOriginalString
+            .indexOf(lowerInputString[lowerInputString.length - 1]);
+      }
+    } else {
+      lastOfInputString = lastOfInputString = lowerOriginalString
+          .indexOf(lowerInputString[lowerInputString.length - 1]);
+    }
+
+    // inputStringと一致する箇所がない場合のエラー(Value not in range: -1)回避
+    if (firstOfInputString == -1) {
+      return [Container()];
+    }
+
+    // 頭から途中まで一致
+    if (lowerOriginalString.startsWith(lowerInputString)) {
+      highlightedText = [
+        Text(
+          originalString.substring(firstOfInputString, lastOfInputString + 1),
+          style: const TextStyle(
+            color: Colors.blue,
+            fontSize: 24.0,
+          ),
+        ),
+        Text(
+          originalString.substring(lastOfInputString + 1),
+          style: TextStyle(fontSize: 24.0),
+        ),
+      ];
+      // 途中で部分一致
+    } else if (!lowerOriginalString.startsWith(lowerInputString) &&
+        !lowerOriginalString.endsWith(lowerInputString) &&
+        lowerOriginalString.contains(lowerInputString)) {
+      highlightedText = [
+        Text(
+          originalString.substring(0, firstOfInputString),
+          style: TextStyle(fontSize: 24.0),
+        ),
+        Text(
+          originalString.substring(firstOfInputString, lastOfInputString + 1),
+          style: const TextStyle(
+            color: Colors.blue,
+            fontSize: 24.0,
+          ),
+        ),
+        Text(
+          originalString.substring(
+              lastOfInputString + 1, lastOfOriginalString + 1),
+          style: TextStyle(fontSize: 24.0),
+        ),
+      ];
+      // 途中から末尾まで一致
+    } else if (lowerOriginalString.endsWith(lowerInputString)) {
+      highlightedText = [
+        Text(
+          originalString.substring(0, firstOfInputString),
+          style: TextStyle(fontSize: 24.0),
+        ),
+        Text(
+          originalString.substring(firstOfInputString, lastOfInputString + 1),
+          style: const TextStyle(
+            color: Colors.blue,
+            fontSize: 24.0,
+          ),
+        ),
+      ];
+      // 完全一致
+    } else if (lowerOriginalString == lowerInputString) {
+      highlightedText = [
+        Text(
+          originalString,
+          style: const TextStyle(
+            color: Colors.blue,
+            fontSize: 24.0,
+          ),
+        ),
+      ];
+    }
+    return highlightedText;
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Highlight Text'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Padding(
+        padding: EdgeInsets.all(15.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            Row(
+              children: <Widget>[
+                const Text(
+                  'Original Text: ',
+                  style: TextStyle(fontSize: 16),
+                ),
+                Flexible(
+                  child: originalText == ''
+                      ? TextField(
+                          controller: _originalTextController,
+                          onEditingComplete: () {
+                            setState(() {
+                              originalText = _originalTextController.text;
+                            });
+                          },
+                        )
+                      : Text(
+                          originalText,
+                          style: const TextStyle(fontSize: 24),
+                        ),
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+            const SizedBox(height: 30.0),
+            Row(
+              children: <Widget>[
+                const Text(
+                  'Highlighted Text: ',
+                  style: TextStyle(fontSize: 16),
+                ),
+                inputText == ''
+                    ? Text(
+                        originalText,
+                        style: const TextStyle(fontSize: 24.0),
+                      )
+                    : Row(
+                        children: getHighlightedText(originalText, inputText),
+                      ),
+              ],
+            ),
+            const SizedBox(height: 60.0),
+            TextField(
+              controller: _highlightedPartController,
+              enabled: originalText == '' ? false : true,
+              decoration: const InputDecoration(hintText: 'Keyword'),
+              onChanged: (value) {
+                setState(() {
+                  inputText = value;
+                });
+              },
+            ),
+            const SizedBox(height: 60.0),
+            RaisedButton(
+              onPressed: refreshScreen,
+              child: const Text('Refresh'),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
