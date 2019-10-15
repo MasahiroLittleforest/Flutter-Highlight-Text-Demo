1e8eedb01783c272346f6b93fe430e25d7c260e9
MasahiroLittleforest <masahiro.littleforest@gmail.com>
2019-10-15 23:02:00 +0900

    fixed function 'getHighlightedText'

--- lib/main.dart
+++ lib/main.dart
@@ -43,101 +43,139 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   List<Widget> getHighlightedText(String originalString, String inputString) {
-    List<Widget> highlightedText;
+    // List<Widget> highlightedText;
     final String lowerOriginalString = originalString.toLowerCase();
     final String lowerInputString = inputString.toLowerCase();
-    final int lastOfOriginalString = originalString.length - 1;
+    // final int lastOfOriginalString = originalString.length - 1;
     final int firstOfInputString =
         lowerOriginalString.indexOf(lowerInputString);
-    int lastOfInputString;
-
-    // 入力値に連続する同じ文字が含まれる時に、'lastOfInputString'が1つ目のインデックスにならないようにする
-    if (lowerInputString.length >= 2) {
-      final String secondLastInputLetter =
-          lowerInputString[lowerInputString.length - 2];
-      final String lastInputLetter =
-          lowerInputString[lowerInputString.length - 1];
-      if (secondLastInputLetter == lastInputLetter) {
-        lastOfInputString = lowerOriginalString.indexOf(lowerInputString) +
+    final int lastOfInputString =
+        lowerOriginalString.indexOf(lowerInputString) +
             (lowerInputString.length - 1);
-      } else {
-        lastOfInputString = lastOfInputString = lowerOriginalString
-            .indexOf(lowerInputString[lowerInputString.length - 1]);
-      }
-    } else {
-      lastOfInputString = lastOfInputString = lowerOriginalString
-          .indexOf(lowerInputString[lowerInputString.length - 1]);
-    }
+    final double _fontSize = 24.0;
+    final Color _highlightColor = Colors.blue;
+    // final String firstInputLetter = lowerInputString[0];
+    // final String lastInputLetter =
+    //     lowerInputString[lowerInputString.length - 1];
+
+    // if (lowerOriginalString.length >= 2 && lowerInputString.length == 1) {
+    //   final String secondLastOriginalLetter =
+    //       lowerOriginalString[lowerOriginalString.length - 2];
+    //   final String lastOriginalLetter =
+    //       lowerOriginalString[lowerOriginalString.length - 1];
+    //   if (secondLastOriginalLetter == lastOriginalLetter) {
+    //     // lastOfInputString = firstOfInputString;
+    //   } else {
+    //     // lastOfInputString = originalString.indexOf(lowerInputString);
+    //   }
+    // }
+
+    // // 入力値に連続する同じ文字が含まれる時に、'lastOfInputString'が1つ目のインデックスにならないようにする
+    // if (lowerInputString.length >= 2) {
+    //   final String secondLastInputLetter =
+    //       lowerInputString[lowerInputString.length - 2];
+    //   if (secondLastInputLetter == lastInputLetter) {
+    //     // lastOfInputString = lowerOriginalString.indexOf(lowerInputString) +
+    //     //     (lowerInputString.length - 1);
+    //   } else {
+    //     // lastOfInputString = lowerOriginalString.indexOf(lowerInputString) +
+    //     //     (lowerInputString.length - 1);
+    //   }
+    // }
 
     // inputStringと一致する箇所がない場合のエラー(Value not in range: -1)回避
-    if (firstOfInputString == -1) {
+    if (firstOfInputString == -1 || lastOfInputString == -1) {
       return [Container()];
     }
 
-    // 頭から途中まで一致
-    if (lowerOriginalString.startsWith(lowerInputString)) {
-      highlightedText = [
-        Text(
-          originalString.substring(firstOfInputString, lastOfInputString + 1),
-          style: const TextStyle(
-            color: Colors.blue,
-            fontSize: 24.0,
-          ),
+    // // 頭から途中まで一致
+    // if (lowerOriginalString.startsWith(lowerInputString)) {
+    //   highlightedText = [
+    //     Text(
+    //       originalString.substring(firstOfInputString, lastOfInputString + 1),
+    //       style: TextStyle(
+    //         color: _highlightColor,
+    //         fontSize: _fontSize,
+    //       ),
+    //     ),
+    //     Text(
+    //       originalString.substring(lastOfInputString + 1),
+    //       style: TextStyle(fontSize: _fontSize),
+    //     ),
+    //   ];
+    //   // 途中で部分一致
+    // } else if (!lowerOriginalString.startsWith(lowerInputString) &&
+    //     !lowerOriginalString.endsWith(lowerInputString) &&
+    //     lowerOriginalString.contains(lowerInputString)) {
+    //   highlightedText = [
+    //     Text(
+    //       originalString.substring(0, firstOfInputString),
+    //       style: TextStyle(fontSize: _fontSize),
+    //     ),
+    //     Text(
+    //       originalString.substring(firstOfInputString, lastOfInputString + 1),
+    //       style: TextStyle(
+    //         color: _highlightColor,
+    //         fontSize: _fontSize,
+    //       ),
+    //     ),
+    //     Text(
+    //       originalString.substring(
+    //           lastOfInputString + 1, lastOfOriginalString + 1),
+    //       style: TextStyle(fontSize: _fontSize),
+    //     ),
+    //   ];
+    //   // 途中から末尾まで一致
+    // } else if (firstOfInputString >= 1 &&
+    //     lastOfInputString == lastOfOriginalString &&
+    //     lowerOriginalString[lowerOriginalString.length - 2] !=
+    //         lowerOriginalString[lowerOriginalString.length - 1]) {
+    //   highlightedText = [
+    //     Text(
+    //       originalString.substring(0, firstOfInputString),
+    //       style: TextStyle(fontSize: _fontSize),
+    //     ),
+    //     Text(
+    //       originalString.substring(firstOfInputString),
+    //       style: TextStyle(
+    //         color: _highlightColor,
+    //         fontSize: _fontSize,
+    //       ),
+    //     ),
+    //   ];
+    //   // 完全一致
+    // } else if (lowerOriginalString == lowerInputString) {
+    //   highlightedText = [
+    //     Text(
+    //       originalString,
+    //       style: TextStyle(
+    //         color: _highlightColor,
+    //         fontSize: _fontSize,
+    //       ),
+    //     ),
+    //   ];
+    // }
+    final List<Widget> highlightedText = [
+      Text(
+        originalString.substring(0, firstOfInputString),
+        style: TextStyle(
+          fontSize: _fontSize,
         ),
-        Text(
-          originalString.substring(lastOfInputString + 1),
-          style: TextStyle(fontSize: 24.0),
+      ),
+      Text(
+        originalString.substring(firstOfInputString, lastOfInputString + 1),
+        style: TextStyle(
+          color: _highlightColor,
+          fontSize: _fontSize,
         ),
-      ];
-      // 途中で部分一致
-    } else if (!lowerOriginalString.startsWith(lowerInputString) &&
-        !lowerOriginalString.endsWith(lowerInputString) &&
-        lowerOriginalString.contains(lowerInputString)) {
-      highlightedText = [
-        Text(
-          originalString.substring(0, firstOfInputString),
-          style: TextStyle(fontSize: 24.0),
+      ),
+      Text(
+        originalString.substring(lastOfInputString + 1),
+        style: TextStyle(
+          fontSize: _fontSize,
         ),
-        Text(
-          originalString.substring(firstOfInputString, lastOfInputString + 1),
-          style: const TextStyle(
-            color: Colors.blue,
-            fontSize: 24.0,
-          ),
-        ),
-        Text(
-          originalString.substring(
-              lastOfInputString + 1, lastOfOriginalString + 1),
-          style: TextStyle(fontSize: 24.0),
-        ),
-      ];
-      // 途中から末尾まで一致
-    } else if (lowerOriginalString.endsWith(lowerInputString)) {
-      highlightedText = [
-        Text(
-          originalString.substring(0, firstOfInputString),
-          style: TextStyle(fontSize: 24.0),
-        ),
-        Text(
-          originalString.substring(firstOfInputString, lastOfInputString + 1),
-          style: const TextStyle(
-            color: Colors.blue,
-            fontSize: 24.0,
-          ),
-        ),
-      ];
-      // 完全一致
-    } else if (lowerOriginalString == lowerInputString) {
-      highlightedText = [
-        Text(
-          originalString,
-          style: const TextStyle(
-            color: Colors.blue,
-            fontSize: 24.0,
-          ),
-        ),
-      ];
-    }
+      ),
+    ];
     return highlightedText;
   }
 
